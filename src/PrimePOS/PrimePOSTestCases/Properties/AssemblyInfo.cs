3f8a22ee68f4dc00da76cb1065b244cdfc227ffe
Sanket Ichale <sichale@micromerchantsystems.com>
2025-06-16 20:03:38 +0530

    added xunit test cases

---
+++ src/PrimePOS/PrimePOSTestCases/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PrimePOSTestCases")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PrimePOSTestCases")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6132381d-6a55-4ca7-8748-20bbb65e2a65")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ src/PrimePOS/PrimePOSTestCases/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Xunit;
+
+namespace PrimePOSTestCases
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Addition_ReturnsCorrectResult()
+        {
+            int a = 5;
+            int b = 3;
+            int sum = a + b;
+
+            Assert.Equal(8, sum);
+        }
+
+        [Fact]
+        public void StringContainsSubstring()
+        {
+            string message = "Welcome to PrimePOS System";
+            Assert.Contains("PrimePOS", message);
+        }
+    }
+}
