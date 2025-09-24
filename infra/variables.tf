c36727c7256363ef68ecd5536797df96db819674
SanketIch <sanket.ichale@ritetechnologies.co.in>
2025-09-24 14:05:38 +0530

    Create variables.tf

---
+++ infra/variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "db_username" {
+  description = "SQL Server admin username"
+}
+
+variable "db_password" {
+  description = "SQL Server admin password"
+}
