2ca182d0a4f606c91508eac7920baf22fcc286b4
SanketIch <sanket.ichale@ritetechnologies.co.in>
2025-10-08 15:06:09 +0530

    Update outputs.tf

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -1,4 +1,14 @@
-output "bucket_name" {
+output "s3_bucket_name" {
   description = "Name of the S3 bucket created"
-  value       = aws_s3_bucket.example.bucket
+  value       = aws_s3_bucket.primepos_bucket.bucket
+}
+
+output "ec2_instance_id" {
+  description = "ID of the EC2 instance created"
+  value       = aws_instance.primepos_ec2.id
+}
+
+output "ec2_public_ip" {
+  description = "Public IP of the EC2 instance"
+  value       = aws_instance.primepos_ec2.public_ip
 }
