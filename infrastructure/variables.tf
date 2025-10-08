6991a2446be3fbdbf0db73ed216f1e569b0f2f1d
SanketIch <sanket.ichale@ritetechnologies.co.in>
2025-10-08 15:06:58 +0530

    Update variables.tf

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,5 +1,16 @@
 variable "aws_region" {
-  description = "AWS region for resources"
+  description = "AWS region to deploy resources"
   type        = string
   default     = "us-east-1"
+}
+
+variable "ec2_key_name" {
+  description = "Name of the existing AWS Key Pair to use for EC2"
+  type        = string
+}
+
+variable "ec2_instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
 }
